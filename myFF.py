4cfc5d1a7299ddc7f510669f0dee70e69a857308
Ansuman Pujari <ansuman.24mas10013@vitbhopal.ap.in>
2024-12-10 16:11:48 +0530

    myFF.py

--- myFF.py
+++ myFF.py
@@ -1,5 +1,8 @@
-iink the Local Repository to GitHub
+iiink the Local Repository to GitHub
 Copy the repository URL from GitHub and run:
     Print("Hello World!")
 :wq
+
+
+my name is ansuman pujari:x
 
